4eb9ae4c5908f6f2455cbdfe8e0de6f95a7e8c53
sbmsr <sebastian.messier@gmail.com>
2021-11-16 19:57:54 -0500

    token failure fix

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except:
 
 if api.requires_2fa:
     print("Two-factor authentication required.")
-    code = input("Enter the code you received of one of your approved devices: ")
+    code = '' + input("Enter the code you received of one of your approved devices: ")
     result = api.validate_2fa_code(code)
     print("Code validation result: %s" % result)
 
